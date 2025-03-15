32b7edc8812a04b88530a1f22813c67ced0450a4
Dieguito <jesusdavidfierrorivera817@gmail.com>
2025-03-15 14:16:58 -0500

    v1

---
+++ variables js/firsth/js/index.js
@@ -0,0 +1,14 @@
+/* 
+ 
+   NAME EXERCISE: FIRST
+   DESCRIPTION: MY FIRST EXERCISE OF JS WITD VARIABLE
+   AUTOR: DIEGUITO
+   DATE: 15 OF MARCH OF 2025 
+*/
+
+let numberOne =  8432
+let numberTwo = 576
+let total;
+total = numberOne+numberTwo;
+
+alert("la suma de sus numeros es igual a: " + total);
---
+++ variables js/second/js/index.js
@@ -0,0 +1,23 @@
+/* 
+ 
+   NAME EXERCISE: SECOND
+   DESCRIPTION: MY SECOND EXERCISE OF JS WITD VARIABLE
+   AUTOR: DIEGUITO
+   DATE: 15 OF MARCH OF 2025 
+*/
+
+let numberOne = 4.5;
+let numberTwo = 2.5;
+let numberThree = 3.5; 
+let weigthed;
+let fullGradeOne;
+let fullGradeTwo;
+let fullGradeThree;
+
+fullGradeOne = numberOne * 0.3;
+fullGradeTwo = numberTwo * 0.4;
+fullGradeThree = numberThree * 0.4;
+
+weigthed = fullGradeOne+fullGradeTwo+fullGradeThree;
+
+console.log("the weigthed is: " + weigthed);
